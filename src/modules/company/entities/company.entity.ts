eab73d953c850cae5489aacdc0a7e8e4d39be78a
junioralcant <junioralcant111@gmail.com>
2022-10-21 10:02:48 -0300

    Update company entity

--- src/modules/company/entities/company.entity.ts
+++ src/modules/company/entities/company.entity.ts
@@ -1,18 +1,22 @@
-import { ICreateCompanyDTO } from '../dtos/icreate-company.dtos';
 import { randomUUID } from 'crypto';
+
+interface ICompanyProps {
+  name: string;
+  cnpj: string;
+}
 
 export class Company {
   id: string;
   name: string;
   cnpj: string;
 
-  private constructor(props: ICreateCompanyDTO) {
+  private constructor(props: ICompanyProps) {
     this.name = props.name;
     this.cnpj = props.cnpj;
     this.id = randomUUID();
   }
 
-  static create(props: ICreateCompanyDTO) {
+  static create(props: ICompanyProps) {
     const company = new Company(props);
     return company;
   }

f4662e36a15cac6ffcc5b52ca648ce261732799d
junioralcant <junioralcant111@gmail.com>
2022-10-21 11:25:50 -0300

    Config parameter required error

--- src/modules/company/usesCases/create-company.usecase.ts
+++ src/modules/company/usesCases/create-company.usecase.ts
@@ -1,19 +1,20 @@
 import { ICreateCompanyDTO } from '../dtos/icreate-company.dtos';
 import { ICompanyRepository } from '../repositories/icompany.repository';
+import { ParameterRequiredError } from '../../../shared/infra/error/parameter-requered.error';
 
 export class CreateCompanyUseCase {
   constructor(private companyRepository: ICompanyRepository) {}
 
   async execute({ name, cnpj }: ICreateCompanyDTO) {
     if (!name || !cnpj) {
-      throw new Error('Name/CNPJ is required');
+      throw new ParameterRequiredError('Name/CNPJ is required', 422);
     }
 
     const companyExisting =
       await this.companyRepository.findByCompanyCNPJ(cnpj);
 
     if (companyExisting) {
-      throw new Error('CNPJ existing.');
+      throw new ParameterRequiredError('CNPJ existing.', 409);
     }
 
     const company = await this.companyRepository.create({

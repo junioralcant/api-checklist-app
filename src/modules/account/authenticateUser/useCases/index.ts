9701948e972e7b8a5f90bcc0068bdd86148399d7
junioralcant <junioralcant111@gmail.com>
2022-10-25 17:06:21 -0300

    Create authenticate user routes

---
+++ src/modules/account/authenticateUser/useCases/index.ts
@@ -0,0 +1,5 @@
+import { AuthenticateUserController } from './authenticate-user.controller';
+
+const authenticateUserController = new AuthenticateUserController();
+
+export { authenticateUserController };
--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import { authenticateUserController } from '../../../../modules/account/authenticateUser/useCases';
 import { createUserController } from '../../../../modules/users/useCases/createUser';
 
 const userRouter = Router();
 
 userRouter.post('/user', (request, response) => {
   createUserController.handle(request, response);
+});
+
+userRouter.post('/user/authenticate', (request, response) => {
+  authenticateUserController.handle(request, response);
 });
 
 export { userRouter };

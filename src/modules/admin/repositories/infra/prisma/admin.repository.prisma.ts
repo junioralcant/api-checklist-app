b2bf84b08666e63eafee6b41bb330396ef8bc5ab
junioralcant <junioralcant111@gmail.com>
2022-10-26 16:28:25 -0300

    Create admin repository prisma

---
+++ src/modules/admin/repositories/infra/prisma/admin.repository.prisma.ts
@@ -0,0 +1,35 @@
+import { prismaClient } from '../../../../../shared/infra/database/prisma.config';
+import { ICreateAdminDTO } from '../../../dtos/icreate-admin.dtos';
+import { Admin } from '../../../entities/admin.entity';
+import { IAdminRepository } from '../../iadmin.repository';
+
+export class AdminRepository implements IAdminRepository {
+  async create({
+    name,
+    email,
+    password,
+  }: ICreateAdminDTO): Promise<Admin> {
+    const admin = await prismaClient.admin.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return admin;
+  }
+
+  async findByAdminEmail(email: string): Promise<Admin | undefined> {
+    const admin = await prismaClient.admin.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: 'insensitive',
+        },
+      },
+    });
+
+    return admin || undefined;
+  }
+}

09f2d1a6f73bff9ffeb04f7c4af0da433279b4e6
junioralcant <junioralcant111@gmail.com>
2022-10-26 16:51:40 -0300

    Create middleware authenticate admin

---
+++ src/shared/infra/http/middleware/ensure.authenticate.admin.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticateAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({
+      message: 'Token missing',
+    });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub } = verify(
+      token,
+      'bc7155981f37d2e86e6f23d1a82bf437'
+    ) as IPayload;
+
+    request.id_admin = sub;
+
+    return next();
+  } catch (error) {
+    return response.status(401).json({
+      message: 'Invalid Token!',
+    });
+  }
+}
